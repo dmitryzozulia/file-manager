38b95e751c4ff7baa38a58fc6754c792e118327c
Dmytro Zozulia <z1dima1z@gmail.com>
2025-05-04 14:16:33 +0200

    feat: add basic operations with files

--- src/commands/handleCommand.js
+++ src/commands/handleCommand.js
@@ -1,8 +1,16 @@
 const { up, cd, ls } = require("./navigation");
+const {
+  readFile,
+  addFile,
+  createDirectory,
+  rename,
+  remove,
+  copy,
+  move,
+} = require("./operations");
 
 async function handleCommand(input, currentDir) {
   const [command, ...args] = input.split(" ");
-  console.log("Command:", command, "Arguments:", args);
   try {
     switch (command) {
       case "ls":
@@ -12,6 +20,27 @@ async function handleCommand(input, currentDir) {
         return up(currentDir);
       case "cd":
         return cd(currentDir, args[0]);
+      case "cat":
+        await readFile(currentDir, args[0]);
+        break;
+      case "add":
+        await addFile(currentDir, args[0]);
+        break;
+      case "mkdir":
+        await createDirectory(currentDir, args[0]);
+        break;
+      case "rn":
+        await rename(currentDir, args[0], args[1]);
+        break;
+      case "rm":
+        await remove(currentDir, args[0]);
+        break;
+      case "cp":
+        await copy(currentDir, args[0], args[1]);
+        break;
+      case "mv":
+        await move(currentDir, args[0], args[1]);
+        break;
       default:
         console.log("Command not found. Please try again.");
         break;
---
+++ src/commands/operations.js
@@ -0,0 +1,138 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+const readFile = async (currentDir, filePath) => {
+  if (!filePath) {
+    console.log("FS operation failed. File path is not provided.");
+    return;
+  }
+  const absolutePath = path.resolve(currentDir, filePath);
+  const readStream = fs.createReadStream(absolutePath);
+  readStream.on("data", (chunk) => {
+    console.log("Reading chunk:", chunk.toString());
+  });
+};
+
+const addFile = async (currentDir, fileName) => {
+  if (!fileName) {
+    console.log("FS operation failed. File name is not provided.");
+    return;
+  }
+  const filePath = path.join(currentDir, fileName);
+  if (fs.existsSync(filePath)) {
+    console.log("FS operation failed. File already exists.");
+  } else {
+    try {
+      await fs.promises.writeFile(filePath, "");
+      console.log(`File ${fileName} created successfully.`);
+    } catch (err) {
+      console.error("Error creating file:", err.message);
+    }
+  }
+};
+
+const createDirectory = async (currentDir, dirName) => {
+  if (!dirName) {
+    console.log("FS operation failed. Directory name is not provided.");
+    return;
+  }
+  const dirPath = path.join(currentDir, dirName);
+  if (fs.existsSync(dirPath)) {
+    console.log("FS operation failed. Directory already exists.");
+  } else {
+    try {
+      await fs.promises.mkdir(dirPath, { recursive: true });
+      console.log(`Directory ${dirName} created successfully.`);
+    } catch (err) {
+      console.error("Error creating directory:", err.message);
+    }
+  }
+};
+
+const rename = async (currentDir, oldName, newName) => {
+  if (!oldName || !newName) {
+    console.log("FS operation failed. Old name or new name is not provided.");
+    return;
+  }
+  if (oldName === newName) {
+    console.log("FS operation failed. Old name and new name are the same.");
+    return;
+  }
+  if (!fs.existsSync(path.join(currentDir, oldName))) {
+    console.log("FS operation failed. File does not exist.");
+    return;
+  }
+  if (fs.existsSync(path.join(currentDir, newName))) {
+    console.log("FS operation failed. New name already exists.");
+    return;
+  }
+  try {
+    await fs.promises.rename(
+      path.join(currentDir, oldName),
+      path.join(currentDir, newName)
+    );
+    console.log(`File renamed from ${oldName} to ${newName} successfully.`);
+  } catch (err) {
+    console.error("Error renaming file:", err.message);
+  }
+};
+
+const remove = async (currentDir, fileName) => {
+  if (!fileName) {
+    console.log("FS operation failed. File name is not provided.");
+    return;
+  }
+  const filePath = path.join(currentDir, fileName);
+  if (!fs.existsSync(filePath)) {
+    console.log("FS operation failed. File does not exist.");
+    return;
+  }
+  try {
+    await fs.promises.unlink(filePath);
+    console.log(`File ${fileName} deleted successfully.`);
+  } catch (err) {
+    console.error("Error deleting file:", err.message);
+  }
+};
+
+const copy = async (currentDir, source, destination) => {
+  if (!source || !destination) {
+    console.log("FS operation failed. Source or destination is not provided.");
+    return;
+  }
+  const sourcePath = path.join(currentDir, source);
+  const destPath = path.join(currentDir, destination);
+  if (!fs.existsSync(sourcePath)) {
+    console.log("FS operation failed. Source file does not exist.");
+    return;
+  }
+  if (fs.existsSync(destPath)) {
+    console.log("FS operation failed. Destination file already exists.");
+    return;
+  }
+  if (sourcePath === destPath) {
+    console.log("FS operation failed. Source and destination are the same.");
+    return;
+  }
+  try {
+    await fs.promises.copyFile(sourcePath, destPath);
+    console.log(`File copied from ${source} to ${destination} successfully.`);
+  } catch (err) {
+    console.error("Error copying file:", err.message);
+  }
+};
+
+const move = async (currentDir, source, destination) => {
+  await copy(currentDir, source, destination);
+  await remove(currentDir, source);
+};
+
+module.exports = {
+  readFile,
+  addFile,
+  createDirectory,
+  rename,
+  remove,
+  copy,
+  move,
+};
